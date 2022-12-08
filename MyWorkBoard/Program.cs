d6e4fa40b466740a1700f027358a24a77845a8c3
Daniel Mrzygłód <daniel.mrz19@gmail.com>
2022-12-08 01:56:20 +0100

    Update, create data to database

--- MyWorkBoard/Program.cs
+++ MyWorkBoard/Program.cs
@@ -9,14 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-
 // 2 sposób po³¹czenia z baz¹ danych
 // Nadpisanie definicji konfiguracji dbcontext na poziomie kontenera dependency injection
 // Generyczny parametr przyjmuje typ danego dbcontextu
 builder.Services.AddDbContext<MyBoardsContext>(
         option => option.UseSqlServer(builder.Configuration.GetConnectionString("MyWorkBoardsConnectionString")));
-
 
 var app = builder.Build();
 
@@ -74,18 +71,94 @@ if (!users.Any())
     dbContext.SaveChanges();
 }
 
-// Wstrzykiwanie MyBoardsContext (jest zarejstrowany wyzej) do endpointu
-app.MapGet("data", (MyBoardsContext db) =>
+// Wstrzykiwanie MyBoardsContext (jest zarejstrowany wyzej) do endpointu. Mapp get -> pobieranie
+app.MapGet("data", async (MyBoardsContext db) =>
     {
         // Najprostsza implementacja metody pobrania tagów z bazy danych i wsadzenia ich do listy
         //var tags = db.Tags.ToList();
         //return tags;
 
-        var epic = db.Epics.First();
-        var user = db.Users.First(u => u.LastName == "Two");
-        return new { epic, user };
+        //var epic = db.Epics.First();
+        //var user = db.Users.First(u => u.LastName == "Two");
+        //return new { epic, user };
+
+        //var toDoWorkItems = db.WorkItems.Where(w => w.StateId == 1).ToList();
+        //return new { toDoWorkItems };
+
+        //var newComments = await db
+        //    .Comments
+        //    .Where(c => c.CreatedDate > new DateTime(2022, 7, 23))
+        //    .ToListAsync();
+        //return newComments;
+
+        //var top5NewestComments = await db.Comments
+        //    .OrderByDescending(c => c.CreatedDate)
+        //    .Take(5)
+        //    .ToListAsync();
+        //return top5NewestComments;
+
+        //var statesCount = await db.WorkItems
+        //    .GroupBy(g => g.StateId)
+        //    .Select(s => new { stateId = s.Key, count = s.Count() })
+        //    .ToListAsync();
+        //return statesCount;
+
+        //var selectedEpics = await db.Epics
+        //.Where(w => w.StateId == 4)
+        //.OrderBy(o => o.Priority)
+        //.ToListAsync();
+        //return selectedEpics;
+
+        var authorsCommentCounts = await db.Comments
+            .GroupBy(g => g.AuthorId)
+            .Select(s => new { s.Key, Count = s.Count() })
+            .ToListAsync();
+
+        // Informacje o liczbie komentarzy
+        var topAuthor =  authorsCommentCounts
+            .First(f => f.Count == authorsCommentCounts.Max(m => m.Count));
+
+        // Szczegó³y o danym u¿ytkowniku
+        var userDetails = db.Users.First(f => f.Id == topAuthor.Key);
+
+        return new { userDetails, commentCount = topAuthor.Count };
     });
 
+// 1 parametr to œcie¿ka pod jak¹ ten endpoint bêdzie dostêpny, 2 parametr to delegata, która obs³u¿y takie zapytanie 
+app.MapPost("update", async (MyBoardsContext db) =>
+    {
+        Epic epic = await db.Epics.FirstAsync(epic => epic.Id == 1);
+
+        var onHoldState = await db.States.FirstAsync(f => f.StateValue == "On Hold");
+
+        //epic.Area = "Updated Area";
+        //epic.Priority = 1;
+        //epic.StartDate = DateTime.Now;
+            
+        epic.StateId = onHoldState.Id;
+
+        await db.SaveChangesAsync();
+
+        return epic;
+    });
+
+app.MapPost("create", async (MyBoardsContext db) =>
+{
+    Tag tag = new Tag()
+    {
+        TagValue = "EF"
+    };
+
+    // 1 sposob
+    // await db.AddAsync(tag);
+
+    // 2 sposob
+    await db.Tags.AddAsync(tag);
+
+    await db.SaveChangesAsync();
+
+    return tag;
+});
 
 app.Run();
 
